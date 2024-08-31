b828196d15e81e6c3aafec4e2cb8554faad03d92
mr-cworld <mcirino@luc.edu>
2024-08-31 11:09:20 -0500

    Cd account completed

--- cd_account.py
+++ cd_account.py
@@ -16,18 +16,23 @@ def create_cd_account(balance, interest_rate, months):
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
     # ADD YOUR CODE HERE
+    user_account = Account(balance, 0)
+    apr = interest_rate
 
     # Calculate interest earned
     # ADD YOUR CODE HERE
+    interest_earned = balance * (apr/100) * (months/12)
 
     # Update the CD account balance by adding the interest earned
-    # ADD YOUR CODE HERE
+    new_balance = balance + interest_earned
 
     # Pass the updated_balance to the set balance method using the instance of the CDAccount class.
     # ADD YOUR CODE HERE
+    user_account.set_balance(new_balance)
 
     # Pass the interest_earned to the set interest method using the instance of the CDAccount class.
     # ADD YOUR CODE HERE
+    user_account.set_interest(new_balance)
 
     # Return the updated balance and interest earned.
-    return  # ADD YOUR CODE HERE
+    return user_account
